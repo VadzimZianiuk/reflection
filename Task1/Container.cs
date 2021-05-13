e8beb154225e6d6d79771ccc731284baaf594899
Vadzim Zianiuk <Vadzim_Zianiuk@epam.com>
2021-05-13 20:18:33 +0300

    add implementation

--- Task1/Container.cs
+++ Task1/Container.cs
@@ -1,28 +1,92 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
+using Task1.DoNotChange;
 
 namespace Task1
 {
     public class Container
     {
+        private readonly Dictionary<Type, Type> container = new Dictionary<Type, Type>();
+
         public void AddAssembly(Assembly assembly)
         {
-            throw new NotImplementedException();
+            foreach (var type in CheckNull(assembly).DefinedTypes)
+            {
+                if (!type.IsAbstract)
+                {
+                    AddType(type, type.GetCustomAttribute<ExportAttribute>(false)?.Contract ?? type);
+                }
+            }
         }
 
-        public void AddType(Type type)
-        {
-            throw new NotImplementedException();
-        }
+        public void AddType(Type type) => AddType(type, type);
 
         public void AddType(Type type, Type baseType)
         {
-            throw new NotImplementedException();
+            if (CheckNull(type).IsAbstract)
+            {
+                throw new ArgumentException("Type is abstract", nameof(type));
+            }
+
+            if (!CheckNull(baseType).IsAssignableFrom(type))
+            {
+                throw new ArgumentException($"{baseType} is not assignable from {type}");
+            }
+
+            if (container.ContainsKey(type))
+            {
+                throw new ArgumentException("Type has already been added.", nameof(type));
+            }
+
+            container.Add(type, type);
+            if (type != baseType)
+            {
+                container.Add(baseType, type);
+            }
         }
 
         public T Get<T>()
         {
-            throw new NotImplementedException();
+            return GetHelper(typeof(T));
+
+            dynamic GetHelper(Type sourceType)
+            {
+                if (!container.TryGetValue(sourceType, out var type))
+                {
+                    throw new ArgumentException($"Type {sourceType} is not added.", nameof(sourceType));
+                }
+
+                var parameterInfos = Array.Empty<ParameterInfo>();
+                _ = type.GetConstructors()
+                    .Any(ci =>
+                    {
+                        parameterInfos = ci.GetParameters();
+                        return parameterInfos.Any(pi => !container.ContainsKey(pi.ParameterType));
+                    });
+
+                var parameters = new object[parameterInfos.Length];
+                for (int i = 0; i < parameterInfos.Length; i++)
+                {
+                    //If we do not check cross-references when adding types, we will most likely end up in an endless loop. 
+                    parameters[i] = GetHelper(parameterInfos[i].ParameterType);
+                }
+                var obj = Activator.CreateInstance(type, parameters);
+
+                foreach (var propertyInfo in type.GetProperties())
+                {
+                    if (propertyInfo.GetCustomAttribute<ImportAttribute>() != null)
+                    {
+                        //If we do not check cross-references when adding types, we will most likely end up in an endless loop. 
+                        propertyInfo.SetValue(obj, GetHelper(container[propertyInfo.PropertyType]));
+                    }
+                }
+
+                return obj;
+            }
         }
+
+        private static T CheckNull<T>(T obj) => obj ?? throw new ArgumentNullException(nameof(obj));
     }
 }
--- Task2/ObjectExtensions.cs
+++ Task2/ObjectExtensions.cs
@@ -1,17 +1,76 @@
 ﻿using System;
+using System.Reflection;
 
 namespace Task2
 {
     public static class ObjectExtensions
     {
-        public static void SetReadOnlyProperty(this object obj, string propertyName, object newValue)
+        private const BindingFlags Flags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.DeclaredOnly;
+
+        public static void SetReadOnlyProperty<T>(this T obj, string propertyName, object newValue)
         {
-            throw new NotImplementedException();
+            if (obj is null)
+            {
+                throw new ArgumentNullException(nameof(obj));
+            }
+            
+            if (propertyName is null)
+            {
+                throw new ArgumentNullException(nameof(propertyName));
+            }
+
+            var type = typeof(T);
+            do
+            {
+                var info = type.GetProperty(propertyName, Flags);
+                if (info != null)
+                {
+                    if (info.CanWrite)
+                    {
+                        info.SetValue(obj, newValue);
+                    }
+                    else
+                    {
+                        (type.GetField($"<{propertyName}>k__BackingField", Flags) ?? throw new InvalidOperationException($"Can't find auto field for {propertyName} property."))
+                            .SetValue(obj, newValue);
+                    }
+                    return;
+                }
+
+                type = type.BaseType;
+            } 
+            while (type != null);
+
+            throw new InvalidOperationException($"Type {typeof(T).FullName} hasn't {propertyName} property.");
         }
 
-        public static void SetReadOnlyField(this object obj, string filedName, object newValue)
+        public static void SetReadOnlyField<T>(this T obj, string filedName, object newValue)
         {
-            throw new NotImplementedException();
+            if (obj is null)
+            {
+                throw new ArgumentNullException(nameof(obj));
+            }
+
+            if (filedName is null)
+            {
+                throw new ArgumentNullException(nameof(filedName));
+            }
+
+            var type = typeof(T);
+            do
+            {
+                var info = type.GetField(filedName, Flags);
+                if (info != null)
+                {
+                    info.SetValue(obj, newValue);
+                    return;
+                }
+
+                type = type.BaseType;
+            } 
+            while (type != null);
+            
+            throw new InvalidOperationException($"Type {typeof(T).FullName} hasn't {filedName} field.");
         }
     }
 }
